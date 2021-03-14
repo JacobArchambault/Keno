34e831a363d4bc26adf7067c6b309a66908b2909
Jacob Archambault <jacobarchambault@gmail.com>
2021-03-14 12:40:51 -0400

    Declare and initialize radio border and spots border

--- src/com/jacobarchambault/keno/Keno.java
+++ src/com/jacobarchambault/keno/Keno.java
@@ -1,13 +1,16 @@
 package com.jacobarchambault.keno;
 
+import javax.swing.BorderFactory;
 import javax.swing.JFrame;
+import javax.swing.border.Border;
 // Jacob Archambault
 // 15 Mar 2021
 // The purpose of this program is to create a workable Keno game
 public class Keno extends JFrame {
+	Border radioBorder = BorderFactory.createTitledBorder("Choose bet amount ($)");
+	Border spotsBorder = BorderFactory.createTitledBorder("Choose numbers to play");
 	
 	public static void main(String[] args) {
-
 	}
 
 }
