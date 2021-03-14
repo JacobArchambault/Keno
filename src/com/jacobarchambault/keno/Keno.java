271cc5244ede48fc60f48e163f6f6104d545290b
Jacob Archambault <jacobarchambault@gmail.com>
2021-03-14 12:36:07 -0400

    Add comments

--- src/com/jacobarchambault/keno/Keno.java
+++ src/com/jacobarchambault/keno/Keno.java
@@ -1,7 +1,9 @@
 package com.jacobarchambault.keno;
 
 import javax.swing.JFrame;
-
+// Jacob Archambault
+// 15 Mar 2021
+// The purpose of this program is to create a workable Keno game
 public class Keno extends JFrame {
 	
 	public static void main(String[] args) {
