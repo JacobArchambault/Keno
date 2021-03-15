68b2933c9e2d201c495ccff413de68c9224040e0
Jacob Archambault <jacobarchambault@gmail.com>
2021-03-14 20:58:41 -0400

    Define createButtons()

--- src/com/jacobarchambault/keno/Keno.java
+++ src/com/jacobarchambault/keno/Keno.java
@@ -1,5 +1,6 @@
 package com.jacobarchambault.keno;
 
+import java.awt.Color;
 import java.awt.Font;
 import java.awt.GridLayout;
 
@@ -12,6 +13,7 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.border.Border;
+
 // Jacob Archambault
 // 15 Mar 2021
 // The purpose of this program is to create a workable Keno game
@@ -20,8 +22,10 @@ public class Keno extends JFrame {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	private Border radioBorder = BorderFactory.createTitledBorder("Choose bet amount ($)");
-	private Border spotsBorder = BorderFactory.createTitledBorder("Choose numbers to play");
+	private Border radioBorder = BorderFactory.createTitledBorder(
+			"Choose bet amount ($)");
+	private Border spotsBorder = BorderFactory.createTitledBorder(
+			"Choose numbers to play");
 	private JButton[] jButtons = new JButton[80];
 	private JPanel buttonPanel1;
 	private JPanel buttonPanel2;
@@ -30,16 +34,28 @@ public class Keno extends JFrame {
 	private JRadioButton[] dollars = new JRadioButton[6];
 	private ButtonGroup spotsGroup = new ButtonGroup();
 	private JButton[] spots = new JButton[12];
-	private Font font = new Font("Arial", Font.BOLD, 38);
-	private Font font2 = new Font("Arial", Font.BOLD, 18);
-	private JLabel luckyLabel = new JLabel(new ImageIcon("images/Lucky.png"));
+	private Font font = new Font(
+			"Arial",
+			Font.BOLD,
+			38);
+	private Font font2 = new Font(
+			"Arial",
+			Font.BOLD,
+			18);
+	private JLabel luckyLabel = new JLabel(
+			new ImageIcon(
+					"images/Lucky.png"));
 
-	public static void main(String[] args) {
+	public static void main(
+			String[] args) {
 		new Keno();
 	}
-	
+
 	public Keno() {
-		this.setLayout(new GridLayout(3, 1));
+		this.setLayout(
+				new GridLayout(
+						3,
+						1));
 		createButtons();
 		createRadioButtons();
 		createPanels();
@@ -47,23 +63,32 @@ public class Keno extends JFrame {
 	}
 
 	private void addPanels() {
-		this.add(buttonPanel1);
-		this.add(middlePanel);
-		this.add(buttonPanel2);
+		this.add(
+				buttonPanel1);
+		this.add(
+				middlePanel);
+		this.add(
+				buttonPanel2);
 	}
 
 	private void createPanels() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	private void createRadioButtons() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	private void createButtons() {
-		// TODO Auto-generated method stub
+		for (int i = 0; i < spots.length; i++) {
+			spots[i] = new JButton(String.valueOf(i + 1));
+			spots[i].setHorizontalAlignment(JButton.CENTER);
+			spots[i].setFont(font);
+			spots[i].setBackground(getBackground().blue);
+			spots[i].setForeground(getForeground().yellow);
+			}
 		
 	}
 
