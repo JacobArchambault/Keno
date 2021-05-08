c52feae31a1f30af9f743d7c04f4160f634db81a
Jacob Archambault <jacobarchambault@gmail.com>
2021-05-07 20:46:54 -0400

    begin adding try/catch

--- src/com/jacobarchambault/keno/Keno.java
+++ src/com/jacobarchambault/keno/Keno.java
@@ -9,9 +9,11 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
+import java.io.FileOutputStream;
 import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Formatter;
 
 import javax.swing.BorderFactory;
 import javax.swing.Box;
@@ -437,13 +439,22 @@ public class Keno extends JFrame {
 					}
 				}
 			});
+			if (!timer.isRunning()) {
+				timer.start();
+			}
+			try {
+				Formatter output = new Formatter(new FileOutputStream("Final.txt", true));
+			} catch (Exception ex) {
+
+			}
+
 		}
 
 	}
 
 	protected void displayWinnings() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
